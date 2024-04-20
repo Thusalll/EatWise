3b7ee8284a2514dbd9c2443a54b86c24f4e5d257
Thusalll <thusalsathnidu@gmail.com>
2024-04-20 20:49:16 +0100

    firebase added to the project

--- EatWise/ContentView.swift
+++ EatWise/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("Nunito-Regular", size: 30))
         }
         .padding()
     }
