3bafc99218f6412413fbf5e5da3b21dd0ae26c48
Thusalll <thusalsathnidu@gmail.com>
2024-04-20 20:10:07 +0100

    Initial Commit

---
+++ EatWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EatWise
+//
+//  Created by Thusal Athauda on 20/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EatWise/EatWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatWiseApp.swift
+//  EatWise
+//
+//  Created by Thusal Athauda on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EatWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
